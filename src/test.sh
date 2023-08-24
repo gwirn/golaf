e206c18032cb3b905065919d01bec7091157795d
gwirn <71886945+gwirn@users.noreply.github.com>
2023-08-24 12:56:20 +0200

    added test(s) files

---
+++ src/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ ! $(go run main.go "line" ../testFiles/* | grep line | wc -l) -eq 6 ]];then
+    echo "FAILED: Pattern matching failed with default settings and word 'line'"
+    exit 1
+else
+    echo "PASS: Pattern matching with default settings and word 'line' "
+fi
+
+if [[ ! $(go run main.go "world" ../testFiles/* | grep world | wc -l) -eq 6 ]];then
+    echo "FAILED: Pattern matching failed with default settings and word 'world'"
+    exit 1
+else
+    echo "PASS: Pattern matching with default settings and word 'world' "
+fi
+
+if [[ ! $(go run main.go -quality 0 "xyz" ../testFiles/* | wc -l) -eq 14 ]];then
+    echo "FAILED: Pattern matching failed where it should match every line"
+    exit 1
+else
+    echo "PASS: Pattern matching with quality 0 "
+fi
+
+if [[ ! $(go run main.go -quality 100 "xyz" ../testFiles/* | wc -l) -eq 2 ]];then
+    echo "FAILED: Pattern matching failed where it should match nothing and print only the file paths"
+    exit 1
+else
+    echo "PASS: Pattern matching for forced no result "
+fi
+
+if [[ ! $(go run main.go -quality 100 "world0" ../testFiles/* | grep world | wc -l) -eq 1 ]];then
+    echo "FAILED: Pattern matching failed where it should match only one line"
+    exit 1
+else
+    echo "PASS: Pattern matching for forced one line result "
+fi
+
+if [[ ! $(cat ../testFiles/testFile1.txt | go run main.go line | grep line | wc -l) -eq 6 ]];then
+    echo "FAILED: Reading from StdIn through pipe failed"
+    exit 1
+else
+    echo "PASS: Pattern matching for input from StdIn "
+fi
